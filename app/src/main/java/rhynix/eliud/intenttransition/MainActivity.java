56143077538c64bcfdc92f7f4cab04cc5bf89843
Eliud Njuguna Kamau <spacetecictsolutions@gmail.com>
2017-05-24 23:45:40 +0300

    initial commit
    This Application will show how to reveal an activity with the circular
    reveal animation. This can be a really nice way to start a new activity
    from a circular button (a floating action button)

---
+++ app/src/main/java/rhynix/eliud/intenttransition/MainActivity.java
@@ -0,0 +1,40 @@
+package rhynix.eliud.intenttransition;
+
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnCreate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnCreate = (Button)findViewById(R.id.btn);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presentActivity(v);
+            }
+
+            private void presentActivity(View view) {
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(MainActivity.this,view,"transition");
+                int revealX = (int)(view.getX() + view.getWidth()/ 2);
+                int revealY = (int)(view.getY() + view.getHeight() / 2);
+
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra(SecondActivity.EXTRA_CIRCULAR_REVEAL_X,revealX);
+                intent.putExtra(SecondActivity.EXTRA_CIRCULAR_REVEAL_Y,revealY);
+
+                ActivityCompat.startActivity(MainActivity.this,intent,options.toBundle());
+            }
+        });
+    }
+}
